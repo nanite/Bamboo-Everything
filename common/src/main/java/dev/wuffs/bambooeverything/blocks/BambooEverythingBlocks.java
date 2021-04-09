aa65bfe6a25b7732edcf2eeafcc6ab9f3dee7e5f
Gareth Williams <gaz492@gmail.com>
2021-04-09 21:06:31 +0100

    More files

--- common/src/main/java/dev/wuffs/bambooeverything/blocks/BambooEverythingBlocks.java
+++ common/src/main/java/dev/wuffs/bambooeverything/blocks/BambooEverythingBlocks.java
@@ -1,2 +1,19 @@
-package dev.wuffs.bambooeverything.blocks;public class BambooEverythingBlocks {
+package dev.wuffs.bambooeverything.blocks;
+
+import dev.wuffs.bambooeverything.BambooEverything;
+import dev.wuffs.bambooeverything.blocks.block.BambooBundleBlock;
+import me.shedaniel.architectury.registry.DeferredRegister;
+import me.shedaniel.architectury.registry.RegistrySupplier;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.block.BannerBlock;
+import net.minecraft.world.level.block.Block;
+
+public class BambooEverythingBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(BambooEverything.MOD_ID, Registry.BLOCK_REGISTRY);
+
+    public static final RegistrySupplier<Block> BUNDLE = BLOCKS.register("bundle", BambooBundleBlock::new);
+
+    public static void register() {
+        BLOCKS.register();
+    }
 }
--- common/src/main/java/dev/wuffs/bambooeverything/blocks/block/BambooBundleBlock.java
+++ common/src/main/java/dev/wuffs/bambooeverything/blocks/block/BambooBundleBlock.java
@@ -1,25 +1,17 @@
 package dev.wuffs.bambooeverything.blocks.block;
 
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import org.jetbrains.annotations.Nullable;
 
-public class BambooBlock extends Block
+public class BambooBundleBlock extends Block
 {
 
-    protected BambooBlock() {
-        super(Properties.of(Material.BARRIER, MaterialColor.COLOR_LIGHT_BLUE)
-                .noOcclusion()
-                .noDrops()
-                .isViewBlocking((a, b, c) -> false)
-                .strength(-1, 6000000F)
-                .lightLevel(blockState -> 3)
-                .emissiveRendering((blockState, blockGetter, blockPos) -> true)
+    public BambooBundleBlock() {
+        super(Properties.of(Material.BAMBOO)
+                .strength(1.0f, 2.0f)
+                .sound(SoundType.BAMBOO)
         );
     }
 }
