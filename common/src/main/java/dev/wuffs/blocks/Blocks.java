f3611297c2a2d8a6c9886640ade8ebda8288d069
Gareth Williams <gaz492@gmail.com>
2022-10-01 16:31:11 +0100

    Added dry varients

--- common/src/main/java/dev/wuffs/blocks/Blocks.java
+++ common/src/main/java/dev/wuffs/blocks/Blocks.java
@@ -12,12 +12,20 @@ public class Blocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(BambooEverything.MOD_ID, Registry.BLOCK_REGISTRY);
 
     public static final RegistrySupplier<Block> BUNDLE = BLOCKS.register("bamboo_bundle", BambooBundleBlock::new);
+    public static final RegistrySupplier<Block> DRY_BUNDLE = BLOCKS.register("dry_bamboo_bundle", BambooBundleBlock::new);
     public static final RegistrySupplier<Block> DOOR = BLOCKS.register("bamboo_door", BambooDoorBlock::new);
+    public static final RegistrySupplier<Block> DRY_DOOR = BLOCKS.register("dry_bamboo_door", BambooDoorBlock::new);
     public static final RegistrySupplier<Block> FENCE = BLOCKS.register("bamboo_fence", BambooFenceBlock::new);
+    public static final RegistrySupplier<Block> DRY_FENCE = BLOCKS.register("dry_bamboo_fence", BambooFenceBlock::new);
     public static final RegistrySupplier<Block> FENCEGATE = BLOCKS.register("bamboo_fence_gate", BambooFenceGateBlock::new);
+    public static final RegistrySupplier<Block> DRY_FENCEGATE = BLOCKS.register("dry_bamboo_fence_gate", BambooFenceGateBlock::new);
     public static final RegistrySupplier<Block> LADDER = BLOCKS.register("bamboo_ladder", BambooLadderBlock::new);
+    public static final RegistrySupplier<Block> DRY_LADDER = BLOCKS.register("dry_bamboo_ladder", BambooLadderBlock::new);
     public static final RegistrySupplier<Block> SLAB = BLOCKS.register("bamboo_slab", BambooSlabBlock::new);
+    public static final RegistrySupplier<Block> DRY_SLAB = BLOCKS.register("dry_bamboo_slab", BambooSlabBlock::new);
     public static final RegistrySupplier<Block> STAIRS = BLOCKS.register("bamboo_stairs", BambooStairsBlock::new);
+    public static final RegistrySupplier<Block> DRY_STAIRS = BLOCKS.register("dry_bamboo_stairs", BambooStairsBlock::new);
     public static final RegistrySupplier<Block> TRAPDOOR = BLOCKS.register("bamboo_trapdoor", BambooTrapDoorBlock::new);
+    public static final RegistrySupplier<Block> DRY_TRAPDOOR = BLOCKS.register("dry_bamboo_trapdoor", BambooTrapDoorBlock::new);
 
 }
--- common/src/main/java/dev/wuffs/items/Items.java
+++ common/src/main/java/dev/wuffs/items/Items.java
@@ -20,13 +20,28 @@ public class Items {
     }
 
     public static final RegistrySupplier<Item> BUNDLE = blockItem("bamboo_bundle", Blocks.BUNDLE);
+    public static final RegistrySupplier<Item> DRY_BUNDLE = blockItem("dry_bamboo_bundle", Blocks.DRY_BUNDLE);
+
     public static final RegistrySupplier<Item> DOOR = blockItem("bamboo_door", Blocks.DOOR);
+    public static final RegistrySupplier<Item> DRY_DOOR = blockItem("dry_bamboo_door", Blocks.DRY_DOOR);
+
     public static final RegistrySupplier<Item> FENCE = blockItem("bamboo_fence", Blocks.FENCE);
+    public static final RegistrySupplier<Item> DRY_FENCE = blockItem("dry_bamboo_fence", Blocks.DRY_FENCE);
+
     public static final RegistrySupplier<Item> FENCEGATE = blockItem("bamboo_fence_gate", Blocks.FENCEGATE);
+    public static final RegistrySupplier<Item> DRY_FENCEGATE = blockItem("dry_bamboo_fence_gate", Blocks.DRY_FENCEGATE);
+
     public static final RegistrySupplier<Item> LADDER = blockItem("bamboo_ladder", Blocks.LADDER);
+    public static final RegistrySupplier<Item> DRY_LADDER = blockItem("dry_bamboo_ladder", Blocks.DRY_LADDER);
+
     public static final RegistrySupplier<Item> SLAB = blockItem("bamboo_slab", Blocks.SLAB);
+    public static final RegistrySupplier<Item> DRY_SLAB = blockItem("dry_bamboo_slab", Blocks.DRY_SLAB);
+
     public static final RegistrySupplier<Item> STAIRS = blockItem("bamboo_stairs", Blocks.STAIRS);
+    public static final RegistrySupplier<Item> DRY_STAIRS = blockItem("dry_bamboo_stairs", Blocks.DRY_STAIRS);
+
     public static final RegistrySupplier<Item> TRAPDOOR = blockItem("bamboo_trapdoor", Blocks.TRAPDOOR);
+    public static final RegistrySupplier<Item> DRY_TRAPDOOR = blockItem("dry_bamboo_trapdoor", Blocks.DRY_TRAPDOOR);
 
     public static final RegistrySupplier<Item> RAFT = ITEMS.register("bamboo_raft", () -> new BambooRaftItem((new Item.Properties().stacksTo(1).tab(BambooEverything.CREATIVE_TAB))));
 
