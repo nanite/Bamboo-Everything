735fd7751e612ee5f909ce5ac6892dcdd9872adc
Gareth Williams <gaz492@gmail.com>
2024-01-07 15:25:16 +0000

    Updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 ## [4.0.0]
 
+- Update neo,forge,fabric loader versions
+
+## [4.0.0]
+
 - Ported to 1.20.4
 
 ## [3.0.2]
--- neoforge/src/main/java/dev/wuffs/forge/BambooEverythingForge.java
+++ neoforge/src/main/java/dev/wuffs/forge/BambooEverythingForge.java
@@ -4,6 +4,7 @@ import dev.architectury.utils.Env;
 import dev.architectury.utils.EnvExecutor;
 import dev.wuffs.BambooEverything;
 import dev.wuffs.BambooEverythingClient;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -11,11 +12,11 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BambooEverything.MOD_ID)
 public class BambooEverythingForge {
-    public BambooEverythingForge() {
+    public BambooEverythingForge(IEventBus modEventBus) {
 //        Event.registerModEventBus(BambooEverything.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         BambooEverything.init();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        modEventBus.addListener(this::clientSetup);
+        modEventBus.addListener(this::commonSetup);
         EnvExecutor.runInEnv(Env.CLIENT, ()-> BambooEverythingClient::init);
     }
 
